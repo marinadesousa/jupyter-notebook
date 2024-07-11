47377bd448914c03c1d05758d6968e61aaaa62fc
marinadesousa <sousamarina224@gmail.com>
2024-07-11 08:59:52 -0300

    Programa finalizadp

--- funcaoTarefas.py
+++ funcaoTarefas.py
@@ -1,14 +1,43 @@
 def adicionarTarefa(tarefas, tarefasDic):
-    tarefa = input("Digite o nome da tarefa para adicionar:")
-    tarefas.append(tarefas)
-    tarefasDic.update({tarefa:"Não Concluída"})
-    
-def removerTarefa (tarefas,tarefasDic):
-    tarefa = input("Digite o nome da tarefa para remover:")
-    tarefas.remove(tarefa)
-    tarefasDic.pop(tarefa)
+    tarefa = input("\nDigite o nome da tarefa para adicionar: ").lower().strip() # Lê a tarefa, transformando em minúsuculas e removendo os espaços iniciais e finais
+    tarefas.append(tarefa) # Armazena a tarefa lida a lista 'tarefas' que será um dos pârametros usado na hora da chamada da função
+    tarefasDic.update({tarefa: "Não Concluída"}) # Armazena a tarefa lida no dicionário 'tarefasDic' já com o valor de 'Não Concluída'
+    return print(f"Tarefa '{tarefa}' adicionada a lista de tarefas!") # Retorno confirmando a inclusão da tarefa
+
+def removerTarefa(tarefas, tarefasDic):
+    tarefa = input("\nDigite o nome da tarefa para remover: ").lower().strip() # Lê a tarefa, transformando em minúsuculas e removendo os espaços iniciais e finais
+    if tarefa in tarefas: # Caso encontre a tarefa lida na lista de tarefas:
+        tarefas.remove(tarefa) # Remove uma tarefa da lista 'tarefas'
+        tarefasDic.pop(tarefa) # Remove uma tarefa do dicionário 'tarefasDic'
+        return print(f"Tarefa '{tarefa}' removida da lista de tarefas!") # Vai retornar confirmando que a tarefa foi excluída
+    else: # Caso não encontre a tarefa lida na lista de tarefas:
+        return print(f"Tarefa '{tarefa}' não encontrada!") # Retorna confirmando que a tarefa não foi encontrada
 
 def conclusaoTarefa(tarefasDic):
-    tarefa = input("Digite o nome para marcar a tarefa que está concluída:")
-    tarefasDic.update ({tarefa:"Concluída"})
-    print(tarefasDic)
+    tarefa = input("\nDigite o nome para marcar a tarefa que está concluída:").lower().strip() # Lê a tarefa que será buscada, transformando em minúsuculas e removendo os espaços iniciais e finais
+    if tarefa in tarefasDic: # Caso encontre a tarefa:
+        tarefasDic.update({tarefa: "Concluída"}) # Marcar como concluída
+        return print(f" '{tarefa}' marcada como concluída!") # Vai retornar confirmando que a tarefa está concluída
+    else: # Caso não encontre a tarefa:
+        return print(f"Tarefa '{tarefa}' não encontrada!") # Vai retornar confirmando que a tarefa não foi encontrada
+    
+
+def listarConcluidas(tarefasDic): 
+    concluidas = [] # Inicia uma lista vazia para armazenar as tarefas que forem concluídas
+    for tarefa, status in tarefasDic.items(): # Usado para cada tarefa e status no dicionário 'tarefasDic':
+        if status == "Concluída": # Verifica se o status de alguma tarefa está como 'Concluída'
+            concluidas.append(tarefa) # Caso esteja concluída, adicione a tarefa a lista de tarefas concluídas
+    print('------Tarefas Concluídas------')
+    for tarefa in concluidas:
+        print(tarefa) # Imprime cada tarefa da lista 'concluidas'
+    print('------------------------------')
+
+def listarNaoConcluidas(tarefasDic):
+    naoConcluidas = [] # Inicia uma lista vazia para armazenar as tarefas que não estão concluídas
+    for tarefa, status in tarefasDic.items(): # Para cada tarefa e status no dicionário 'tarefasDic':
+        if status == "Não Concluída": # Verifica se o status de uma tarefa está como 'Não Concluída'
+            naoConcluidas.append(tarefa) # Caso não esteja concluída, adicione a tarefa na lista de tarefas não concluídas
+    print('------Tarefas Não Concluídas------')
+    for tarefa in naoConcluidas:
+        print(tarefa) # Imprime cada tarefa da lista 'naoConcluidas'
+    print('------------------------------')
--- gerenciamentodeTarefas.py
+++ gerenciamentodeTarefas.py
@@ -1,5 +1,5 @@
 import funcaoTarefas as ft
-print('Menu') 
+print('\n------------------Menu------------------') 
 print ('''
     1. Adicionar Tarefa
     2. Remover Tarefa
@@ -12,22 +12,38 @@ print ('''
 
 tarefas = []
 tarefasDic = {}
+opcoesValidas = ("1", "2", "3", "4", "5", "6", "7")
 sair = False
 
 while sair == False:
-    opcao = int(input('Escolha a opção que deseja: '))
-    if opcao == 1:
-        ft.adicionarTarefa(tarefas, tarefasDic)
-        print(tarefas)
-    
-    if opcao == 2:
-        ft.removerTarefa(tarefas, tarefasDic)
+    opcao = input("\nEscolha uma opção: ").strip()
+
+    if opcao not in opcoesValidas:
+        print("Opção inválida, digite um número entre 1 e 7!\n")
+    else: 
+        opcao = int(opcao)
+
+        if opcao == 1:
+            ft.adicionarTarefa(tarefas, tarefasDic)
+
+        if opcao == 2:
+            ft.removerTarefa(tarefas, tarefasDic)
+
+        if opcao == 3:
+            ft.conclusaoTarefa(tarefasDic)
+
+        if opcao == 4:
+            print('------Minhas Tarefas------')
+            for tarefa, status in tarefasDic.items():
+                print(f"{tarefa}: {status}")
+            print('--------------------------')
         
-    if opcao == 3:
-        ft.conclusaoTarefa(tarefasDic)
+        if opcao == 5:
+            ft.listarConcluidas(tarefasDic)
         
-    if opcao == 4:
-        print(tarefas)
-        
-    if opcao == 7:
-        sair = True
+        if opcao == 6:
+            ft.listarNaoConcluidas(tarefasDic)
+
+        if opcao == 7:
+            print("O Programa está finalizado")
+            sair = True
